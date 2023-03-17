78f6ac3a97c7442c09cf46c5fe2e34fd8edcab3b
maridoroshuk <m.doroshuk@bk.ru>
2023-03-17 16:54:02 +0300

    refactor: add type button

--- src/components/ButtonWithIcon/index.tsx
+++ src/components/ButtonWithIcon/index.tsx
@@ -9,7 +9,7 @@ export class ButtonWithIcon extends Component<IButtonWithIcon> {
     const { children, icon, onButtonClick } = this.props;
 
     return (
-      <button className="icon-button" onClick={onButtonClick}>
+      <button className="icon-button" type="button" onClick={onButtonClick}>
         {children}
         <span className="icon-button-icon" style={{ backgroundImage: `url(${icon})` }}></span>
       </button>
